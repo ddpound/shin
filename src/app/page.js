26755905f1f385ad5d5cc7518a2cc94ded674fae
dpound@naver.com <dpound@naver.com>
2023-11-29 22:03:25 +0900

    메인 페이지 디자인 수정중 컨셉및 뼈대 레이아웃 작성

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,30 @@
 export default function Home() {
   return (
     <main className="main-div">
-      main page
+      <div className="main-left-div">
+        <div>
+          <label className="main-title-label">Furniture.</label>
+          <p className="main-explanation-label">Find out the most efficient way to start</p>
+          <p className="main-explanation-label">organizing your home. Begin by creating</p>
+          <p className="main-explanation-label">storage space.</p>
+        </div>
+        <div className="main-left-image">
+          left div image or playvideo
+        </div>
+      </div>
+      <div className="main-center-div">
+        <div className="center-in-img-div">
+          image div
+        </div>
+        <div className="center-in-explanation-div">
+          01.<br/> A beatifull chair <br/>
+          A collection of simple, minimal <br/>
+          chairs and seating
+        </div>
+      </div>
+      <div className="main-right-div">
+        right div
+      </div>
     </main>
   )
 }
