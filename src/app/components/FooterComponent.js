c9aa018be104e97667c804a73dfedf0166b51e02
dpound@naver.com <dpound@naver.com>
2023-12-07 22:03:08 +0900

    푸터 디자인 완료

--- src/app/components/FooterComponent.js
+++ src/app/components/FooterComponent.js
@@ -3,13 +3,24 @@ function FooterComponent(){
         <>
             <footer>
                 <div className="footer-first-content-div">
-                    16Day You can return for free
+                    <div>
+                        <label className="footer-point-font">16</label>
+                        <label>DAYS</label>
+                    </div>
+                    <div>You can return for free</div> 
                 </div>
-                <div className="footer-second-content-div">
-                    4-5 DAYS The maximum
+                <div className="footer-first-content-div">
+                    <div>
+                        <label className="footer-point-font">4-5</label>
+                        <label>DAYS</label>
+                    </div>
+                    <div>The maximum delivery time</div> 
                 </div>
                 <div className="footer-search-content-div">
-                    검색
+                    <div className="footer-search-box">
+                        <input type="text" />
+                        <img src="/logo/search-icon.png"/>
+                    </div>
                 </div>
                 <div className="footer-ins-logo-div">
                     <img src="/logo/twitter.png" />
