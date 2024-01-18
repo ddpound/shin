8bf173551af9f5e53d700948e4046a5a5cdffe73
YouSeongJung <seongjung.you@chemtopia.net>
2024-01-18 12:58:29 +0900

    예약 사이트 페이지 뼈대 구성

--- src/app/store/page.js
+++ src/app/store/page.js
@@ -1,7 +1,13 @@
-export default function styleView(){
+export default function styleView() {
     return (
         <div className="main-div">
             Store 입니다.
+            <div>
+                예약
+            </div>
+            <div>
+                인스타
+            </div>
         </div>
     );
 }
